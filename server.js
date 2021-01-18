737a1f929cbb96c14fb25033aca529a86a8fec2e
Eric Chen <ericchen201896@gmail.com>
2021-01-17 23:39:02 -0800

    Added in .get for /api/notes to reads json file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,14 +8,21 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes",(req,res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("*",(req,res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (error, data) =>
+        error ? res.json(error) : res.json(data)
+    );
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
